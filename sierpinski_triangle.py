a36d01a4d476fa601891a3b8009e924f991270b9
ilidan16 <gta20022016@gmail.com>
2020-11-08 21:17:40 +0300

    Update sierpinski_triangle.py

--- sierpinski_triangle.py
+++ sierpinski_triangle.py
@@ -5,6 +5,8 @@ import random
 
 t.penup()
 t.speed(0)
+t.tracer(1000,0)
+t.hideturtle()
 
 A = [-300,-170]
 B = [150,340]
@@ -13,20 +15,20 @@ O = [0,0]
 
 t.goto(O[0],O[1])
 
-for i in range(3000):
+for i in range(10000):
     x = t.xcor()
     y = t.ycor()
     r = random.randint(1,3)
     
     if r == 1:
         t.goto((x+A[0])/2, (y+A[1])/2)
-        t.dot(3)
+        t.dot(2,"red")
         
     if r == 2:
         t.goto((x+B[0])/2, (y+B[1])/2)
-        t.dot(3)
+        t.dot(2,"blue")
         
     if r == 3:
         t.goto((x+C[0])/2, (y+C[1])/2)
-        t.dot(3)
+        t.dot(2,"green")
 
