cc4fd153bce65777010dc4d8e674b1847adcc8c5
ilidan16 <gta20022016@gmail.com>
2020-11-08 22:00:23 +0300

    Update sierpinski_triangle.py

--- sierpinski_triangle.py
+++ sierpinski_triangle.py
@@ -16,20 +16,24 @@ O = [0,0]         # начальная точка черепашки
 
 t.goto(O[0],O[1])
 
+col = ["red","blue","green"]
+print(col[0])
+
 for _ in range(7000):
     x = t.xcor()
     y = t.ycor()
     r = random.randint(1,3)
+    e = random.randint(0,2)
     
     if r == 1:
         t.goto((x+A[0])/2, (y+A[1])/2)
-        t.dot(2,"red")
+        t.dot(2,col[e])
         
     if r == 2:
         t.goto((x+B[0])/2, (y+B[1])/2)
-        t.dot(2,"blue")
+        t.dot(2,col[e])
         
     if r == 3:
         t.goto((x+C[0])/2, (y+C[1])/2)
-        t.dot(2,"green")
+        t.dot(2,col[e])
 
