bce6e107aa37fcd7f70555ecffa762dd5a6be8f1
ilidan16 <gta20022016@gmail.com>
2020-11-12 09:06:05 +0300

    .

--- fractals/koch_snowflake.py
+++ fractals/koch_snowflake.py
@@ -1,17 +1,23 @@
 # Снежинка Коха
 import turtle
 
+turtle.setup(1440,900)
 turtle.speed(0)
-turtle.tracer(10,0)
+turtle.tracer(50,0)
 turtle.hideturtle()
-turtle.penup()
-turtle.goto(-300,-170)
-turtle.pendown()
 turtle.bgcolor("black")
 turtle.color("blue")
 
-LENGTH = 600 # длина стороны прав. треугольника
-n = 5  #количество итераций 
+LENGTH = 200 # длина стороны прав. многоугольника
+k = 5  #количество итераций в кривой Коха
+n = 6  # количесвто сторон прав. n-угольника
+inside = True # True - рисовать кривую Коха внутри многоугольника
+              # False - рисовать снаружи
+x = 0         # начальные координаты
+y = -300
+turtle.penup()
+turtle.goto(x,y)
+turtle.pendown()
 #===========================================
 # Кривая Коха
 def koch_curve(LENGTH, n):
@@ -27,13 +33,15 @@ def koch_curve(LENGTH, n):
         koch_curve(LENGTH / 3, n - 1)
         
 #===========================================
-turtle.left(60)
-koch_curve(LENGTH, n)
-turtle.left(240)
-koch_curve(LENGTH, n)
-turtle.left(240)
-koch_curve(LENGTH, n)
-
+if inside:
+    for _ in range(n):
+        koch_curve(LENGTH, k)
+        turtle.left(360/n)
+else:
+    turtle.left(180*(n-2)/n)
+    for _ in range(n):
+        koch_curve(LENGTH, k)
+        turtle.right(360/n)
 
 
 
