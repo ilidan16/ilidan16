fb3bc98b281e9b3f1ef62ff472be15bd48a993e4
ilidan16 <gta20022016@gmail.com>
2020-11-13 17:47:20 +0300

    Update tree.py

--- tree.py
+++ tree.py
@@ -5,22 +5,27 @@
     https://www.youtube.com/watch?v=mAz46Z5curo&t=175s"""
 
 """L - система. Дерево Пифагора.
-* переменные: 0, 1, 2
-* константы: [, ], 2
+(Эти правила описывают борщевик, который мы рисовали в классе)
+* переменные: 0, 1
+* константы: [, ]
 * аксиома: 0
 * правила:
-    1. 1 -> 21
+    1. 1 -> 11
     2. 0 -> 1[0]0
 Команды для черепашки:
 0: рисуй отрезок, кончающийся листом
 1: рисуй отрезок
-2: тоже рисует отрезок как 1, но является константой
 [: положи в стек положение и угол рисования, поверни влево
 ]: выбери из стека положение и угол, поверни вправо
 
-2 была добавлена, чтобы при увелечении
-количества итераций длина отрезка
-увеличивалась не в два раза, а увеличивалась на одну единицу"""
+Однака правила будут немного отличаться:
+    1. 1 -> 21
+    2. 0 -> 1[20]20
+2 - это то же самое что и 1, но при отображение она не изменяется (2 -> 2)
+Это даст следующие эффекты:
+1) Длина отрезка будет увеличиваться не в два раза, а всего на одну еденицу
+2) Функция 2 (как и 1) будет игнорироваться с определённой вероятностью,
+в итоге длина листьев будет разной"""
 
 import turtle as t
 import random
@@ -28,15 +33,15 @@ import random
 """Данная функция создаёт строку функций (команд),
 по которым потом черепашка будет рисовать дерево"""
 def create_string_of_functions(iterations):
-    str_current = "0" # аксиома
+    str_current = "22220" # аксиома
     str_new = ""
     for _ in range(iterations):
         for ch in str_current:
-            if ch == "0":           # 2-ое правило
-                str_new += "1[0]0"
+            if ch == "0":           # 2-ое правило 
+                str_new += "1[20]20" 
             if ch == "1":           # 1-ое правило
                 str_new += "21"
-            if ch == "2":           # # константы не трогаем
+            if ch == "2":           # константы не трогаем
                 str_new += "2"
             if ch == "[":           
                 str_new += "["
@@ -52,52 +57,65 @@ t.setup(1440, 900)
 t.hideturtle()
 t.speed(0)
 t.tracer(0,0)
-t.width(0)
 t.penup()
-t.goto(0, -300)
+t.goto(0, -350)
 t.pendown()
 
-iterations = 11
-alpha = 20
+iterations = 12
+alpha = 16
 length = 10
-width_0 = 16 #начальная толщина
+
 def draw_tree():
     t.setheading(90)
+    thickness = 16 #начальная толщина
+    t.pensize(thickness)
     """Будем записывать значения в массив,
 а затем использовать последний элемент и
 сразу удалять его"""
     x = []
     y = []
     angle = []
-    width = []
+    array_thickness = []
     for ch in create_string_of_functions(iterations):
-        if ch == "1":
+        if ch == "1" and random.randint(0,100) > 40:    # стебель
             t.forward(length)
 
-        """Команду 2 будем выполнять с некоторой вероятностью"""
         if ch == "2" and random.randint(0,100) > 40:
             t.forward(length)
             
-        if ch == "0":
-            t.pencolor("green")
-            t.forward(length)
+        if ch == "0":                                   # лист
+            array_thickness.append(t.pensize())
+            t.pensize(4)
+            r = random.randint(1,3)
+            if r == 1:
+                t.pencolor("#009900")
+            if r == 2:
+                t.pencolor("#667900")
+            if r == 3:
+                t.pencolor("#20BB00")
+            t.forward(length - 2)
+            t.pensize(array_thickness.pop())
             t.pencolor("black")
             
-        if ch == "[":
+        if ch == "[":                       # запоминаем данные перед поворотом налево
+            thickness = thickness * 3/4  
+            t.pensize(thickness)
+            array_thickness.append(thickness)
             x.append(t.xcor()) 
             y.append(t.ycor()) 
-            angle.append(t.heading()) 
+            angle.append(t.heading())
             t.left(alpha + random.randint(-13, 13))
             
-        if ch == "]":
+        if ch == "]":                       #возвращаем данные и поворачиваем направо
             t.penup()
             t.goto(x.pop(), y.pop())
             t.pendown()
+            thickness = array_thickness.pop()
+            t.pensize(thickness)
             t.setheading(angle.pop())
             t.right(alpha + random.randint(-13, 13))
 #============================================================
 draw_tree()
-print (create_string_of_functions(iterations))
 
 
             
