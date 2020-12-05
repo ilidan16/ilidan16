a96bfaa44a90b6c73dcd6c40fc204bdae1beddbd
ilidan16 <gta20022016@gmail.com>
2020-12-05 23:47:07 +0300

    .

--- Matrix.py
+++ Matrix.py
@@ -10,84 +10,65 @@ def create_null_matrix(m,n):
     for i in range(m):
         null_matrix.append(strings)
     return Matrix(null_matrix)
-
+#==============================================================
 class Matrix():
     ''' Класс матриц размером m x n'''
-    def __init__(self, matrix=0):
-        self.matrix = matrix
-        if self.matrix == 0:
-            raise MatrixError('Матрица не определена')
-        self.m = len(self.matrix)
-        self.n = len(self.matrix[0])
+    def __init__(self, body=0):
+        if body == 0:
+            raise MatrixError('Вы не определили матрицу')
+        self.body = body
+        self.m = len(body)
+        self.n = len(body[0])
 
     def __str__(self):
         string = ''
-        for i in range(len(self.matrix)): 
-            for j in range(len(self.matrix[i])):
-                string += f'{self.matrix[i][j]} '
+        for i in range(self.m): 
+            for j in range(self.n):
+                string += f'{self.body[i][j]} '
             string += '\n'
         return string
 
     def __add__(self, other):
         if self.m != other.m or self.n != other.n:
             raise MatrixError('Матрицы имеют разный размер')
-        matrix1 = self.matrix.copy()
-        matrix_other = other.matrix.copy()
-        m = self.m
-        n = self.n
-        null_matrix = create_null_matrix(m,n)
-        for i in range(m):
-            for j in range(n):
-                null_matrix.matrix[i][j] = matrix1[i][j] + matrix_other[i][j]
+        matrix1 = self.body.copy()
+        other_matrix = other.body.copy()
+        null_matrix = create_null_matrix(self.m,self.n)
+        for i in range(self.m):
+            for j in range(self.n):
+                null_matrix.body[i][j] = matrix1[i][j] + other_matrix[i][j]
         return null_matrix
-
-
+    
     def __mul__(left_matrix, right_matrix):
         if left_matrix.n != right_matrix.m:
             raise MatrixError('Произведение для таких матриц не определено')
-        left_matrix_copy = left_matrix.matrix.copy()
-        right_matrix_copy = right_matrix.matrix.copy()
-        result = create_null_matrix(left_matrix.m, right_matrix.n).matrix
+        left_matrix_copy = left_matrix.body.copy()
+        right_matrix_copy = right_matrix.body.copy()
+        result = create_null_matrix(left_matrix.m, right_matrix.n).body
         for i in range(left_matrix.m):
             for j in range(right_matrix.n):
                 column = []
                 for q in range(right_matrix.m):
                     column.append(right_matrix_copy[q][j])
-                array = []
                 for k, p in zip(left_matrix_copy[i], column):
-                    result[i][j] += k*p
-                   
+                    result[i][j] += k*p     
         return Matrix(result)
-                
-                
-                
         
         
+
+
 
 
 
 A = Matrix([
+    [5,3,6],
+    [4,2,1],
+    [2,5,9]
+    ])
+
+B = Matrix([
     [1,0,0],
     [0,1,0],
     [0,0,1]
     ])
-
-B = Matrix([
-    [2,3,5],
-    [2,6,4],
-    [8,9,7]
-    ])
-C = Matrix([
-    [1,1,1],
-    [1,1,1],
-    [1,1,1]
-    ])
-
-D = Matrix([
-    [1,1,1],
-    [1,1,1],
-    [1,1,1]
-    ])
-
-
-
+        
