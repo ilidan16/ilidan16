cb5ff3f23689592440c7770cfd94841bc412cc7b
ilidan16 <gta20022016@gmail.com>
2021-03-06 10:08:08 +0300

    Update mandelbrot_parallel.py

--- mandelbrot_parallel.py
+++ mandelbrot_parallel.py
@@ -6,7 +6,7 @@ import matplotlib.colors as clr
 
 from threading import Thread
 #===================================================
-# https://mandel.gart.nz/
+# Поиск красивых мест: https://mandel.gart.nz/
 """
 x = -1.748187465
 y = 0.001251896
@@ -23,16 +23,16 @@ xmax = 1.5
 ymin = -2
 ymax = 2
 
-m = 10
-n = 10
+m = 500
+n = 500
 itr = 200
 border = 2.0
 #===================================================
-image = np.zeros((10, 10))
+image = np.zeros((m, n))
 image1 = np.zeros((m, int(n/2)))
 image2 = np.zeros((m, int(n/2)))
 def mandelbrot(xmin,xmax,m,n,image_i, ymin=ymin, ymax=ymax, itr=itr, border=border):
-    image = image1
+    image = image_i
     for i_y, y in enumerate(np.linspace(ymin, ymax, n)):
         for i_x, x in enumerate(np.linspace(xmin, xmax, m)):
             c = x + y * 1j
@@ -47,20 +47,17 @@ half_x = (xmin + xmax)/2
 
 t0 = time.time()
 if __name__ == '__main__':
-    t1 = Thread(target=mandelbrot, args=(xmin, half_x, m, int(n/2), image1))
-    t2 = Thread(target=mandelbrot, args=(half_x, xmax, m, int(n/2), image2))
+    t1 = Thread(target=mandelbrot, args=(xmin, half_x, m, int(n/2), image1,))
+    t2 = Thread(target=mandelbrot, args=(half_x, xmax, m, int(n/2), image2,))
     
     t1.start()
     t2.start()
 
-    t1.join()
-    t2.join()
+    t1.join(1)
+    t2.join(1)
 print(time.time() - t0)
 
-print(image1)
-print(image2)
-#image = np.concatenate((image1,image2), axis=1)
-#print(image)
 
-#mandelbrot(xmin,xmax,m,n,image, ymin=ymin, ymax=ymax, itr=itr, border=border)
-
+t0 = time.time()
+mandelbrot(xmin,xmax,m,n,image, ymin=ymin, ymax=ymax, itr=itr, border=border)
+print(time.time() - t0)
