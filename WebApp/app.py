711f31f61ed9744a524b40f32917575b5612d6b9
ilidan16 <gta20022016@gmail.com>
2021-04-20 09:07:38 +0300

    /

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,31 +1,28 @@
 from flask import Flask, render_template, request, flash
-
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+#import semester_1.primality_test as test
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
 
 @app.route('/')
 def index():
-    return render_template('index.html', menu=menu)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('index.html')
 
 
-@app.route('/contact', methods=["POST", 'GET'])
-def contact():
-
-
+@app.route('/simplicity', methods=["POST", 'GET'])
+def simplicity():
     if request.method == 'POST':
-        if len(request.form['username']) > 2:
+        number = request.form['enter']
+
+        if len(request.form['enter']) > 2:
             flash('Сообщение отправлено')
         else:
             flash('Ошибка отправки')
 
         
-    return render_template('contact.html')
+
+    
+    return render_template('simplicity.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
