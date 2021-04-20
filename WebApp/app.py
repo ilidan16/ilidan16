d0f1aaf72e4605befa1dfb8dea4c6e64c9d3f082
ilidan16 <gta20022016@gmail.com>
2021-04-20 08:25:20 +0300

    app

---
+++ WebApp/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print( url_for('index') )
+    return render_template('index.html', menu=menu)
+
+@app.route('/about')
+def about():
+    print( url_for('about') )
+    return render_template('about.html')
+
+
+@app.route("/profile/<path:username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+if __name__ == "__main__":
+    app.run(debug = True)
